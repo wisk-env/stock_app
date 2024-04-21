e9b95abae7ac73121c3028db11105d5894d855ba
wisk-env <wiskenv566@gmail.com>
2024-04-21 22:57:34 +0900

    Stocksテーブルにストックの期限のデータを保持するカラムを追加 #7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_21_051304) do
+ActiveRecord::Schema.define(version: 2024_04_21_135137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2024_04_21_051304) do
     t.bigint "item_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "start_time"
     t.index ["item_id"], name: "index_stocks_on_item_id"
     t.index ["user_id"], name: "index_stocks_on_user_id"
   end
