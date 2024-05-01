d8bd1c6970286e9cb8a6dd2476d7333137172ae5
wisk-env <wiskenv566@gmail.com>
2024-05-02 00:18:15 +0900

    ストック商品に設定されているカテゴリーからストックを検索する機能を作成 #9

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -6,6 +6,7 @@ class StocksController < ApplicationController
   def index
     @user = User.find(current_user.id)
     @stocks = @user.stocks.order('id DESC')
+    @stock_categories = @user.stocks.select(:category).distinct
   end
 
   def show
