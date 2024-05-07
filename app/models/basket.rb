ab8098c79e487cc2d5435be4bb67b9a9a27dbdcf
wisk-env <wiskenv566@gmail.com>
2024-05-07 18:23:06 +0900

    basket.rbの修正:空のデータが保存されないようにバリデーションの設定

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,4 +1,8 @@
 class Basket < ApplicationRecord
+  validates :product_name, presence: true
+  validates :product_qty, presence: true, numericality: true
+  validates :due_date, presence: true
+
   belongs_to :stock
   belongs_to :user
 end
