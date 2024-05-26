aff18761fff81dfa6c7ad8826f8876ac61fd0d04
wisk-env <wiskenv566@gmail.com>
2024-05-26 19:31:17 +0900

    グループとユーザーを関連付けるための中間テーブル UserGroupモデルを作成する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_22_134740) do
+ActiveRecord::Schema.define(version: 2024_05_26_042006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,16 @@ ActiveRecord::Schema.define(version: 2024_05_22_134740) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_groups", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "family_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["family_id"], name: "index_user_groups_on_family_id"
+    t.index ["user_id", "family_id"], name: "index_user_groups_on_user_id_and_family_id", unique: true
+    t.index ["user_id"], name: "index_user_groups_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -108,4 +118,6 @@ ActiveRecord::Schema.define(version: 2024_05_22_134740) do
   add_foreign_key "stock_tags", "users"
   add_foreign_key "stocks", "items"
   add_foreign_key "stocks", "users"
+  add_foreign_key "user_groups", "families"
+  add_foreign_key "user_groups", "users"
 end
