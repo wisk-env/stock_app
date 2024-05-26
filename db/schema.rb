aba80c46a121c5284a19b03216942bfb878a8e40
wisk-env <wiskenv566@gmail.com>
2024-05-26 12:06:54 +0900

    Familyモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_07_135707) do
+ActiveRecord::Schema.define(version: 2024_05_22_134740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 2024_05_07_135707) do
     t.datetime "updated_at", null: false
     t.index ["stock_id"], name: "index_baskets_on_stock_id"
     t.index ["user_id"], name: "index_baskets_on_user_id"
+  end
+
+  create_table "families", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "items", force: :cascade do |t|
