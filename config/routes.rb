654f7d01af1938d8c97c3277fd77d46fa16e7171
wisk-env <wiskenv566@gmail.com>
2024-06-08 16:10:53 +0900

    routingの設定

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
-    passwords: 'users/passwords'
+    passwords: 'users/passwords',
+    invitations: 'users/invitations'
   }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
